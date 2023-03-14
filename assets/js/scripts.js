d0a928cf3f3695b0f0c2b26bd96e3df97072a1b4
samiraomidvar7129 <93695009+samiraomidvar7129@users.noreply.github.com>
2023-03-14 02:49:17 -0700

    add jquery file

---
+++ assets/js/scripts.js
@@ -0,0 +1,16 @@
+$('.section').hide();
+// sideNav
+$('.button-collapse').sideNav();
+
+// counter
+$('.count').each(function(){
+    $(this).prop('Counter',0).animate({
+        Counter:$(this).text();
+    },{
+        duration:3000,
+        easing:'swing',
+        step:function(now){
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
