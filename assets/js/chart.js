b2740cbb2b9869ea8865615e28e6b265506ea807
samiraomidvar7129 <93695009+samiraomidvar7129@users.noreply.github.com>
2023-03-14 07:39:20 -0700

    attach ckeditor cdn

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,46 +1,35 @@
-window.onload = function () {
+
+window.onload = function() {
  
     var chart = new CanvasJS.Chart("chartContainer", {
-        animationEnabled: true,  
-        title:{
-            text: "Company Revenue by Year"
-        },
-        axisY: {
-            title: "Revenue in USD",
-            valueFormatString: "#0,,.",
-            suffix: "mn",
-            prefix: "$"
+        theme: "light2", // "light1", "light2", "dark1", "dark2"
+        exportEnabled: true,
+        animationEnabled: true,
+        title: {
+            text: "Desktop Browser Market Share in 2016"
         },
         data: [{
-            type: "splineArea",
-            color: "rgba(54,158,173,.7)",
-            markerSize: 5,
-            xValueFormatString: "YYYY",
-            yValueFormatString: "$#,##0.##",
+            type: "pie",
+            startAngle: 25,
+            toolTipContent: "<b>{label}</b>: {y}%",
+            showInLegend: "true",
+            legendText: "{label}",
+            indexLabelFontSize: 16,
+            indexLabel: "{label} - {y}%",
             dataPoints: [
-                { x: new Date(2000, 0), y: 3289000 },
-                { x: new Date(2001, 0), y: 3830000 },
-                { x: new Date(2002, 0), y: 2009000 },
-                { x: new Date(2003, 0), y: 2840000 },
-                { x: new Date(2004, 0), y: 2396000 },
-                { x: new Date(2005, 0), y: 1613000 },
-                { x: new Date(2006, 0), y: 2821000 },
-                { x: new Date(2007, 0), y: 2000000 },
-                { x: new Date(2008, 0), y: 1397000 },
-                { x: new Date(2009, 0), y: 2506000 },
-                { x: new Date(2010, 0), y: 2798000 },
-                { x: new Date(2011, 0), y: 3386000 },
-                { x: new Date(2012, 0), y: 6704000},
-                { x: new Date(2013, 0), y: 6026000 },
-                { x: new Date(2014, 0), y: 2394000 },
-                { x: new Date(2015, 0), y: 1872000 },
-                { x: new Date(2016, 0), y: 2140000 }
+                { y: 51.08, label: "Chrome" },
+                { y: 27.34, label: "Internet Explorer" },
+                { y: 10.62, label: "Firefox" },
+                { y: 5.02, label: "Microsoft Edge" },
+                { y: 4.07, label: "Safari" },
+                { y: 1.22, label: "Opera" },
+                { y: 0.44, label: "Others" }
             ]
         }]
-        });
-        setTimeout(function(){
-            chart.render();
-
-        })
+    });
+    
+    setTimeout(function(){
+        chart.render();
+    })
      
-    }
+    }  
